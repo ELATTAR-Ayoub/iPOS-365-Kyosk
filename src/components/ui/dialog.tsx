6f5c8fa441f4200331d9a051d98f8c1bb56b31da
ELATTAR-Ayoub <elattarayoub000@gmail.com>
2024-12-30 16:12:56 +0100

    small color changes - 4

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed center-in-screen  kiosk:top-[150px] kiosk:max-h-[calc(100%-300px)] kiosk:translate-y-0 z-50 grid w-[calc(100%-50px)] sm:max-w-lg kiosk:max-w-none max-h-[95%] sm:max-h-[80%]  gap-4 border bg-secondary p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
+        "fixed center-in-screen  kiosk:top-[150px] kiosk:max-h-[calc(100%-300px)] kiosk:translate-y-0 z-50 grid w-[calc(100%-100px)] sm:max-w-lg kiosk:max-w-none max-h-[95%] sm:max-h-[80%]  gap-4 border bg-secondary p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
         className
       )}
       {...props}
